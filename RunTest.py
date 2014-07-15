bf2512bb8512a673f76e4509e6408ee25828a2b0
NickMD <mader.nikita@gmail.com>
2014-07-14 23:36:06 -0700

    Update RunTest.py

--- RunTest.py
+++ RunTest.py
@@ -5,6 +5,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 import time
 import datetime
 from WelcomePage import welcomePage
+import json
+from pprint import pprint
+from urllib2 import urlopen
 #from selenium.webdriver.common.by import By
 
 #from selenium.webdriver.support import expected_conditions as EC
@@ -50,7 +53,7 @@ class Test(unittest.TestCase):
         wait = WebDriverWait(self.driver, 10)  # Wait 10 second for element present
         wait.until(lambda driver: driver.find_elements_by_id("id_fname"))
         welcome = welcomePage(self.driver)
-        welcome.login(self.valid_fname,self.valid_lname,self.valid_email,self.valid_phone,self.genderM,self.california)
+
         assert self.driver.title in self.expectedTitle
         self.driver.close()
 
@@ -106,7 +109,6 @@ class Test(unittest.TestCase):
         self.driver.close()
         self.driver.switch_to_window(self.driver.window_handles[0])
 
-
     def testIsPresentDayInFooter(self):
         """
         TC-01.10
@@ -116,6 +118,43 @@ class Test(unittest.TestCase):
         siteDay = self.driver.find_element_by_id("timestamp").text
         realDay = ("%s %s, %s" % (time.strftime("%B"), i.day, i.year))
         self.assertIn(realDay,siteDay)
+        self.driver.close()
+
+    def testWeatherValidation(self):
+        """
+        TC-01.15
+        Check weather is present and actual
+        """
+        geopluginurl = "http://geoplugin.net/json.gp?ip=76.126.119.162" # TODO: {ip} format
+        jsonUrlGeo = urlopen(geopluginurl)
+        jsonObjGeoLoaded = json.load(jsonUrlGeo)
+        #print jsonObjGeoLoaded
+        location = (jsonObjGeoLoaded["geoplugin_latitude"],jsonObjGeoLoaded["geoplugin_longitude"])
+        wundergroundApi = "http://api.wunderground.com/api/8a75c2aa5ba78758/conditions/q/{0[0]},{0[1]}.json".format(location)
+        urlOpenWunderground = urlopen(wundergroundApi)
+        jsonObjWundergroundLoaded = json.load(urlOpenWunderground)
+        temperatureJson = str(jsonObjWundergroundLoaded["current_observation"]['temp_f'])               # Temp from webservice
+        cityStateJson = jsonObjWundergroundLoaded["current_observation"]["display_location"]['full']    # City,State from webservice
+        iconJson = jsonObjWundergroundLoaded["current_observation"]['icon_url']                         # Icon Weather from webservice
+        temperatureSite = self.driver.find_element_by_id("id_temperature").text
+        cityStateSite = self.driver.find_element_by_id("id_current_location").text
+        self.assertIn(temperatureJson,temperatureSite)
+        self.assertIn(cityStateJson,cityStateSite)
+        self.driver.close()
+
+    def testQuoteBetween63and103(self):
+        """
+        TC-01.16
+        Quote is displayed and quotes: not less 67 and not more 103 chars
+        """
+        quotelen = len(self.driver.find_element_by_id(self.id_quotes).text)
+        quotetext = self.driver.find_element_by_id(self.id_quotes).text
+        try:
+            quotelen < 103 and quotelen > 67
+        except:
+            print "Quote is out of range between 63 and 103. Quote text:", quotetext
+
+
 
 
 
